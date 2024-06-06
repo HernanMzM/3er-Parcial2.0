ceebcef402ec4820813d0d87ad353cdf0efadb4a
HernanMzM <hernanmum3@gmail.com>
2024-06-06 12:53:06 -0600

    elevar un numero a una potencia sin math

---
+++ prog24.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+	int num, pot, res=1,i,co;
+	cout<<"Ingrese el numero que quiere elevar";
+	cin>>num;
+	cout<<"Ingrese a que potencia desea elevarlo";
+	cin>>pot;
+	for(i=0; i<pot; i++){
+		res*=num;
+	}
+	cout<<"el numero elevado al cuadrado "<<res<<"\n";
+}
