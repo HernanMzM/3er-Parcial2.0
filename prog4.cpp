c0821eecfc1089375b4469266a5e5d39a212c229
HernanMzM <hernanmum3@gmail.com>
2024-06-06 10:52:43 -0600

    identifica si es vocal

---
+++ prog4.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main (){
+	char c;
+	cout<<"ingrese una letra\n";
+    cin>>c;
+    switch (c)
+    {
+        case'a':
+        cout<<c<<" es una vocal";
+        break;
+        case'e':
+        cout<<c<<" es una vocal";
+        break;
+        case'i':
+        cout<<c<<" es una vocal";
+        break;
+        case'o':
+        cout<<c<<" es una vocal";
+        break; 
+        case'u':
+        cout<<c<<" es una vocal";
+        break;
+        default:
+        cout<<"no es una vocal";
+        break;
+    }
+}
