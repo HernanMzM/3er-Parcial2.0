23bbf98e944a4bf3c96afc47555f862f0dc4fced
HernanMzM <hernanmum3@gmail.com>
2024-06-06 19:39:15 -0600

    28 extendido

--- prog27.cpp
+++ prog27.cpp
@@ -2,21 +2,23 @@
 #include <string.h>
 using namespace std;
 int main(){
-	char pass[12],name[20];
-	int co,i,precio,cantidad,total,co2;
+	char pass[12],root[20];
+	int co,i;
 	co=0;
+
 	do{
 		co=co+1;
-		cout<<"usuario:\n";
-		cin>>name;
-		cout<<"contraseña:\n";
+		cout<<" nombre de user ";
+		cin>>root;
+		cout<<" contrasena ";
 		cin>>pass;
-		if(strcmp(pass,"2b26")==0 && strcmp(name,"Hernan")==0){
+		if(strcmp(pass,"1234")==0 && strcmp(root,"root")==0){
 			cout<<"Bienvenido al sistema\n";
 			break;
 		}
 			if (co >= 3) {
         cout<<"Has excedido el número máximo de intentos\n";
-    	}
-    }while(co<3);
+    }
+	}while(co<3);
+	cout<<"numero de intentos realizados "<<co;
 }
